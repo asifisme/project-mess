88f3d00cc2b909489616d45aefed54bcaf7a9106
asif <asif192001@gmail.com>
2023-08-08 04:18:09 -0500

    Start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1 className="text-3xl text-center bg-slate-500">Hello world!</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
